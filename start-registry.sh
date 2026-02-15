d8da20857669ff0554ab00af8dac6e8e45e5befb
Brandon Weber <brandon.weber@nymbl.app>
2026-02-15 11:35:51 -0500

    Add Railway start scripts

---
+++ start-registry.sh
@@ -0,0 +1,1 @@
+cd registry && npm install && npm run prisma:generate && npm run build && npm run prisma:push && npm run prisma:seed && npm start
---
+++ start-validator.sh
@@ -0,0 +1,1 @@
+cd validator && npm install && npm run build && npm run start:web
